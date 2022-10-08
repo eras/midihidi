6ea44fce429a1afc3ec0fc56c927f158a31544fc
Erkki Seppälä <erkki.seppala@vincit.fi>
2022-10-08 17:42:11 +0300

    ready

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod error;
 
+use input_linux::{
+    EventKind, EventTime, InputEvent, InputId, KeyEvent, KeyState, SynchronizeEvent,
+    SynchronizeKind, UInputHandle,
+};
+use nix::libc::O_NONBLOCK;
+use std::fs::OpenOptions;
+use std::os::unix::fs::OpenOptionsExt;
+use std::sync::mpsc::sync_channel;
 use std::sync::{Arc, Mutex};
-use std::{thread, time};
 
 struct State {}
 
@@ -25,8 +32,52 @@ fn init_midi() -> (
     )
 }
 
+fn make_uhandle() -> UInputHandle<std::fs::File> {
+    let uinput_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .custom_flags(O_NONBLOCK)
+        .open("/dev/uinput")
+        .expect("cannot read uinput");
+    let uhandle = UInputHandle::new(uinput_file);
+
+    uhandle.set_evbit(EventKind::Key).unwrap();
+    uhandle.set_keybit(input_linux::Key::Q).unwrap();
+
+    let input_id = InputId {
+        bustype: input_linux::sys::BUS_USB,
+        vendor: 0x1234,
+        product: 0x5678,
+        version: 0,
+    };
+    let device_name = b"midihid";
+    uhandle
+        .create(&input_id, device_name, 0, &[])
+        .expect("Failed to create device");
+    uhandle
+}
+
 fn main() {
     let (client, midi_receiver, _state) = init_midi();
+
+    let uhandle = make_uhandle();
+
+    const ZERO: EventTime = EventTime::new(0, 0);
+    let events = [
+        *InputEvent::from(KeyEvent::new(
+            ZERO,
+            input_linux::Key::Q,
+            KeyState::pressed(true),
+        ))
+        .as_raw(),
+        *InputEvent::from(KeyEvent::new(
+            ZERO,
+            input_linux::Key::Q,
+            KeyState::pressed(false),
+        ))
+        .as_raw(),
+        *InputEvent::from(SynchronizeEvent::new(ZERO, SynchronizeKind::Report, 0)).as_raw(),
+    ];
 
     let jack_callback = {
         //let state = Arc::clone(&state);
@@ -37,7 +88,10 @@ fn main() {
             for event in midi.iter(ps) {
                 match event.bytes {
                     [248] => (),
-                    _ => println!("{event:?}"),
+                    _ => {
+                        println!("{event:?}");
+                        //uhandle.write(&events).expect("Failed to write events");
+                    }
                 }
             }
             jack::Control::Continue
@@ -75,20 +129,20 @@ fn main() {
     }
 
     if true {
-        let _active_client = client
+        let active_client = client
             .activate_async((), jack::ClosureProcessHandler::new(jack_callback))
             .expect("Failed to activate client");
 
-        // measurement_fin_rx
-        //     .recv()
-        //     .expect("Failed to received measurement finish response")?;
+        let (break_tx, break_rx) = sync_channel(1);
+        ctrlc::set_handler(move || {
+            break_tx.send(()).unwrap();
+        })
+        .expect("Error setting Ctrl-C handler");
 
-        loop {
-            thread::sleep(time::Duration::from_secs(10));
-        }
+        break_rx.recv().expect("Failed to receive ctrl-c");
 
-        // active_client
-        //     .deactivate()
-        //     .expect("Failed to deactivate Jack session");
+        active_client
+            .deactivate()
+            .expect("Failed to deactivate Jack session");
     }
 }
